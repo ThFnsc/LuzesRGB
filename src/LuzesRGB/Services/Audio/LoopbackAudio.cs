4155f919324ee87186fde4b121f658947a11925d
Thiago Fonseca da Silveira <thiago.fonseca@umbler.com>
2020-09-06 23:20:29 -0300

    Detect silence and reset loopback

--- src/LuzesRGB/Services/Audio/LoopbackAudio.cs
+++ src/LuzesRGB/Services/Audio/LoopbackAudio.cs
@@ -12,36 +12,23 @@ namespace LuzesRGB
 {
     class LoopbackAudio : IAudioProvider
     {
+        private WasapiLoopbackCapture _audio;
+        private BufferedWaveProvider _bwp;
+        private DateTime? _lastSilence;
+        private static readonly int BUFFER_SIZE = (int)Math.Pow(2, 15);
+        private static readonly TimeSpan MAX_SILENCE = TimeSpan.FromSeconds(10);
+
         [DllImport("shlwapi.dll")]
         public static extern int ColorHLSToRGB(int H, int L, int S);
 
-        readonly WasapiLoopbackCapture audio;
-        readonly BufferedWaveProvider bwp;
         public event EventHandler<float[]> OnAudioData;
-        int samplesCaptured = 0;
-        DateTime started = DateTime.MinValue;
-        readonly int BUFFER_SIZE = (int)Math.Pow(2, 15);
-
-        public LoopbackAudio()
-        {
-            audio = new WasapiLoopbackCapture();
-            bwp = new BufferedWaveProvider(audio.WaveFormat) { DiscardOnBufferOverflow = true, BufferLength = BUFFER_SIZE * 2 };
-            audio.DataAvailable += (sndr, args) => {
-                bwp.AddSamples(args.Buffer, 0, args.BytesRecorded);
-                Calculate();
-            };
-        }
 
         private void Calculate()
         {
-            if (bwp.BufferedBytes < BUFFER_SIZE)
+            if (_bwp.BufferedBytes < BUFFER_SIZE)
                 return;
-            samplesCaptured++;
-            if (started == DateTime.MinValue)
-                started = DateTime.Now;
-            double perSec = samplesCaptured / (DateTime.Now - started).TotalSeconds;
             byte[] buffer = new byte[BUFFER_SIZE];
-            bwp.Read(buffer, 0, BUFFER_SIZE);
+            _bwp.Read(buffer, 0, BUFFER_SIZE);
             float[] leftSamples = new float[BUFFER_SIZE / 8];
             float[] rightSamples = new float[BUFFER_SIZE / 8];
             float[] joinedSamples = new float[BUFFER_SIZE / 8];
@@ -68,16 +55,39 @@ namespace LuzesRGB
             return fft;
         }
 
-        public void Dispose()
+        public void Dispose() =>
+            Task.WhenAny(Task.Run(()=> _audio?.Dispose()), Task.Delay(10000)).Wait();
+
+        public Task Start()
         {
-            if (audio.CaptureState == NAudio.CoreAudioApi.CaptureState.Capturing)
-                audio.StopRecording();
+            _audio = new WasapiLoopbackCapture();
+            _bwp = new BufferedWaveProvider(_audio.WaveFormat) { DiscardOnBufferOverflow = true, BufferLength = BUFFER_SIZE * 2 };
+            _audio.DataAvailable += (sndr, args) =>
+            {
+                _bwp.AddSamples(args.Buffer, 0, args.BytesRecorded);
+                Calculate();
+                if (args.BytesRecorded == 0)
+                {
+                    if (_lastSilence == null)
+                        _lastSilence = DateTime.Now;
+                    else if (DateTime.Now - _lastSilence > MAX_SILENCE)
+                    {
+                        _lastSilence = null;
+                        Stop();
+                        Start();
+                    }
+                }
+                else
+                    _lastSilence = null;
+            };
+            _audio.StartRecording();
+            return Task.CompletedTask;
         }
 
-        public Task Start() =>
-            Task.Run(()=> audio.StartRecording());
-
-        public Task Stop() =>
-            Task.Run(() => audio.StopRecording());
+        public Task Stop()
+        {
+            Dispose();
+            return Task.CompletedTask;
+        }
     }
 }
