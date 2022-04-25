818b61db8138603a26e5c7cc7ebf775e7ad642eb
Thiago Fonseca <thfnsc@outlook.com>
2022-04-25 00:41:27 -0300

    Remove legacy version

--- src/LuzesRGB/Extensions/ColorExtensions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace System.Drawing
-{
-    public static class ColorExtensions
-    {
-        public static Color SetMaxBrightness(this Color input, byte max) =>
-            Color.FromArgb(input.R.Map(0, 255, 0, max), input.G.Map(0, 255, 0, max), input.B.Map(0, 255, 0, max));
-    }
-}
--- src/LuzesRGB/Extensions/NumberExtensions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace System
-{
-    public static class NumberExtensions
-    {
-        public static byte Map(this byte x, byte in_min, byte in_max, byte out_min, byte out_max) =>
-            (byte) ((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min);
-    }
-}
